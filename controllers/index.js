a609c07c067b4e2b72481e453bfad7e66c14a828
Vaibhav Desai <vaibhav.desai@yale.edu>
2016-11-17 00:32:58 +0000

    hide old events

--- controllers/index.js
+++ controllers/index.js
@@ -6,11 +6,19 @@ var events = require('../models/events');
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var now = new Date();
   var contextData = {
     'title': 'MGT 656',
     'tagline': 'You are a rockstar (just kidding).',
-    'events': events.all
+    'events': []
   };
+  console.log(events.all);
+  for(var i=0; i<events.all.length; i++){
+    var event = events.all[i];
+    if(event.date > now){
+      contextData.events.push(event);
+    }
+  }
   response.render('index.html', contextData);
 }
 
